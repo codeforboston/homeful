db9dce7fc0e3d30eb30c5c836afed27224cecaeb
beechnut <cloydster@gmail.com>
2014-05-31 14:12:38 -0400

    Add basic controllers for user types

---
+++ app/controllers/partners_controller.rb
@@ -0,0 +1,18 @@
+class PartnersController < ApplicationController
+  def show
+    @partner = Partner.find params[:id]
+  end
+
+  def new
+    @partner = Partner.new
+  end
+
+  def create
+    # TODO: Create Partner
+  end
+
+  def update
+    @partner = Partner.find params[:id]
+    # TODO: Update Partner
+  end
+end
