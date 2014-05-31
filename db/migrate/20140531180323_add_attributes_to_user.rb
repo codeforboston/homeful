0e5797f342bb9caa46d1b0fb9c610d8e0fdd49de
Emily Williams <ecwilliams66@gmail.com>
2014-05-31 17:20:37 -0400

    delete migrations that are not needed

--- db/migrate/20140531180323_add_attributes_to_user.rb
+++
@@ -1,7 +0,0 @@
-class AddAttributesToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :full_name, :string
-    add_column :users, :nickname, :string
-    add_column :users, :photo, :string
-  end
-end
