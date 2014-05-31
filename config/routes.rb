2ab60237e9cf4a97717bb97aa52d84381aacb10b
beechnut <cloydster@gmail.com>
2014-05-31 14:21:26 -0400

    Make routes concise

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  get 'partners/show'
-
-  get 'partners/new'
-
-  get 'partners/create'
-
-  get 'partners/update'
-
-  get 'donors/show'
-
-  get 'donors/new'
-
-  get 'donors/create'
-
-  get 'donors/update'
-
-  get 'clients/show'
-
-  get 'clients/new'
-
-  get 'clients/create'
-
-  get 'clients/update'
-
+  resources :clients
+  resources :donors
+  resources :partners
 end
