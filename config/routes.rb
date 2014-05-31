b52789c5f23539e00aef89defd3304acf11c2f99
Rachael Serur <rachaelse@gmail.com>
2014-05-31 17:06:54 -0400

    We added Bootstrap with SASS

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
+  get 'static_pages/search'
+
   devise_for :users
 
   resources :clients
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20140531180323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "hstore"
 
   create_table "donations", force: true do |t|
     t.decimal  "amount"
