ebabdada19ad181d3a83c4ac49824335d84fc483
Emily Williams <ecwilliams66@gmail.com>
2014-05-31 17:22:14 -0400

    Created associations! :thumbsup:

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,6 +1,5 @@
 class Donation < ActiveRecord::Base
 
   belongs_to :donor
-  belongs_to :client
   belongs_to :goal
 end
--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,2 +1,3 @@
 class Partner < User
+  has_many :clients, :class_name => "Client", :foreign_key => :partner_id
 end
