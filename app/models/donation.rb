d1cbce51e4cbc0a1d7784780db01284c7b06df81
craigmcginley <craigmcginley@gmail.com>
2014-06-01 11:55:58 -0400

    Added description method to donation model

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -2,4 +2,9 @@ class Donation < ActiveRecord::Base
 
   belongs_to :donor
   belongs_to :goal
+
+  def description
+    "#{donor} gave $#{amount} to #{goal.client} towards their #{goal} goal."
+  end
+
 end
